ef045de41356367d4272b6f2b0a233092a1334fe
Francisco Rafart <francisco.rafart@gmail.com>
2019-06-21 17:31:19 -0400

    Implement recursive function

--- euler31.py
+++ euler31.py
@@ -16,38 +16,22 @@
 
 # How do we know when a number of coins has reached all its possible combinations?
 # There should only be one combination for a determinate number of coins ?
+PENCE_VALUE_MAPPING = {
+    '7': 1,
+    '6': 2,
+    '5': 5,
+    '4': 10,
+    '3': 20,
+    '2': 50,
+    '1': 100,
+    '0': 200,
+}
 
-def coin_combination(value, number_of_coins, start_coin_index, coin_bag= [0,0,0,0,0,0,0,0]):
-
-    current_coin_index = start_coin_index
-
-    # Fill the bag
-    for i in range(number_of_coins):
-        this_coin_bag[current_coin_index]=this_coin_bag[current_coin_index]+1
-        bag_total = coinBagToNumber(this_coin_bag)
-        if bag_total == 200:
-            return this_coin_bag
-        if bag_total < 200:
-            return # coin_combination(200, number_of_coins)
-        if bag_total>200:
-            return # coin_combination(200, number_of_coins, )
-
-    # returns the coin combination as an array or None
-    return None
-
-# We know whe maximum amount of coins is 200
-result = 0
-for i in range(200):
-    combination = coin_combination(200, i+1, 0)
-    if combination:
-        result+=1
-
-# Takes array
 def coinBagToNumber(coinBag):
     coin_total = 0
-    for index, value in coinBag:
+    for index, value in enumerate(coinBag):
         p_amount = PENCE_VALUE_MAPPING[str(index)] or 0
-        coint_total+=p_amount*value
+        coin_total+=p_amount*value
 
     return coin_total
 
@@ -61,21 +45,49 @@ def addsTwo(numbers):
 
     return false
 
+def coin_combination(value, number_of_coins, coin_index = 0, coin_bag= [0,0,0,0,0,0,0,0]):
+    reminder = value
+    this_coin_bag = coin_bag.copy()
 
-PENCE_VALUE_MAPPING = {
-    '7': 1,
-    '6': 2,
-    '5': 5,
-    '4': 10,
-    '3': 20,
-    '2': 50,
-    '1': 100,
-    '0': 200,
-}
+    if reminder == 0 and number_of_coins==0:
+        return this_coin_bag
+
+    coin_value = PENCE_VALUE_MAPPING[str(coin_index)]
+
+    if coin_value == reminder and number_of_coins == 1:
+        # Fill the bag
+        this_coin_bag[coin_index] = coin_bag[coin_index] + 1 # Add one coin on index
+        return this_coin_bag
+
+    if coin_value==reminder and number_of_coins>1 and coin_index<7:
+        return coin_combination(reminder, number_of_coins, coin_index+1, this_coin_bag)
+
+    if coin_value>reminder and number_of_coins>1 and coin_index<7:
+        return coin_combination(reminder, number_of_coins, coin_index+1, this_coin_bag)
+
+    if coin_value < reminder and number_of_coins>0:
+        this_coin_bag[coin_index] = coin_bag[coin_index] + 1 # Fill the bag
+        reminder = reminder-coin_value # substract coin amount
+        return coin_combination(reminder, number_of_coins-1, coin_index, this_coin_bag)
+
+    # returns the coin combination as an array or None
+    return None
+
+# We know whe maximum amount of coins is 200
+result = 0
+for i in range(5):
+    combination = coin_combination(200, i+1)
+    coin_addition = 0
+    if combination is not None:
+        coin_addition = coinBagToNumber(combination)
+    print ('combination: {} for {} coins, adds: {}'.format(combination, i+1, coin_addition))
+    if combination:
+        result+=1
+
 
 # Idea 2
 # with 102 coins combination is 100x1p & 2x50p
-# with 90 coins (90/102 == 2.22) (78*2p)156 + 27()+ (10p 5p 1p) 17     
+# with 90 coins (90/200 == 2.22) (78*2p)156 + 27()+ (10p 5p 2p) 17
 # Divide 200 in the number of coins (ex. 200/102 coins == 1.9)
     # If division is one of our coins fill bag with that one, if not pick the
     # value that follows our division downwards
